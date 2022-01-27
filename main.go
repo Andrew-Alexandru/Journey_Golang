e980cb220ab91d32e019e29202c2f571033a3f83
Andrei-Alexandru Cimpoes <andrei.cimpoes@fortech.ro>
2022-01-27 17:26:06 +0200

    Added pointer and scan function for the user input

--- main.go
+++ main.go
@@ -2,25 +2,38 @@ package main
 
 import "fmt"
 
+//format package - fmt
+
 func main() {
 	conferenceName := "Go Conference"
 	//applies to variables not to constants
 	//var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 	//%T - stands for Type and %v stands for value
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	//ask user for name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
 
-	userName = "Harry"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - uint(userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
 }
